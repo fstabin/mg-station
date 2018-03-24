ef867a7ddbd4d8c440aeb7b19ca8fc993e2ec756
fs_nanamedou <34233189+fstabin@users.noreply.github.com>
2018-03-24 15:38:21 +0900

    タイマー更新

--- fsw/fps_keeper.h
+++ fsw/fps_keeper.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <chrono>
 #include<algorithm>
+#include <thread>
 
 #include "AppBase.h"
 
@@ -16,7 +17,8 @@ namespace game_util {
 			tp = std::chrono::system_clock::now();
 		}
 		bool keepTime() {
-			bool result = AppBase::AppWait(static_cast<acs::ulong>(std::max(0ll, 1000 / 60 - 1 - std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - tp).count())));
+			bool result = AppBase::AppWait(0);
+			std::this_thread::sleep_for(std::chrono::milliseconds(std::max(0ll, 1000 / 60 - 1 - std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - tp).count())));
 			tp = std::chrono::system_clock::now();
 			return result;
 		}
